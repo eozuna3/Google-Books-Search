5dfdcb3c68d0af5cb6c30fc6ca94329b270c1039
eozuna3 <eo30239@gmail.com>
2020-07-02 22:17:08 -0500

    Created a build version of the project.

---
+++ client/build/precache-manifest.d49755626d2b7512959132c8c33809a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8bc4c247fb163e40251",
+    "url": "/static/css/main.f2a2364a.chunk.css"
+  },
+  {
+    "revision": "a8bc4c247fb163e40251",
+    "url": "/static/js/main.2387807e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d180343cf88cf364af2d",
+    "url": "/static/js/2.705f9bb4.chunk.js"
+  },
+  {
+    "revision": "1f3f3a3af7c646a232e498e39c4e4e7b",
+    "url": "/index.html"
+  }
+];
